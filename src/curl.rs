15a015e01c62d6a4418a5a9b1fde00d52ff52c10
Paul Hendry <paul@pshendry.com>
2014-10-12 22:01:35 -0400

    Implemented HTTP GET and POST, and started on structs for the login API call

--- src/curl.rs
+++ src/curl.rs
@@ -10,13 +10,14 @@ pub fn http_get(url: &str) -> IoResult<String> {
     let url_obj = Url::parse(url).unwrap();
     let request: RequestWriter = try!(RequestWriter::new(Get, url_obj));
     let mut response = try!(request.read_response().map_err(|(_request, error)| error));
-
-    match response.read_to_string() {
-        Ok(result) => Ok(result),
-        Err(error) => { return Err(error) },
-    }
+    response.read_to_string()
 }
 
-#[test]
-fn dummy_test2() {
+pub fn http_post(url: &str, data: &[u8]) -> IoResult<String> {
+    let url_obj = Url::parse(url).unwrap();
+    let mut request: RequestWriter = try!(RequestWriter::new(Get, url_obj));
+    request.headers.content_length = Some(data.len());
+    try!(request.write(data));
+    let mut response = try!(request.read_response().map_err(|(_request, error)| error));
+    response.read_to_string()
 }
--- src/planets_nu.rs
+++ src/planets_nu.rs
@@ -1,6 +1,46 @@
+struct RGB {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+struct PlayerSettings {
+    player_planet_colors: (RGB, RGB),
+    enemy_planet_colors: (RGB, RGB),
+    ally_planet_colors: (RGB, RGB),
+    info_planet_colors: (RGB, RGB),
+    unknown_planet_colors: (RGB, RGB),
+    player_ship_colors: (RGB, RGB),
+    enemy_ship_colors: (RGB, RGB),
+    ally_ship_colors: (RGB, RGB),
+    player_mine_color: RGB,
+    enemy_mine_color: RGB,
+    web_mine_color: RGB,
+    ally_mine_color: RGB,
+    ion_storm_color: RGB,
+    assistant_enabled: bool,
+    mouse_zoom_enabled: bool,
+    sound_effects_enabled: bool,
+    music_enabled: bool,
+    battle_task_id: int,
+    battle_tutorial_id: int,
+    id: int,
+}
+
+struct LoginRequest {
+    username: &str,
+    password: &str,
+}
+
+struct LoginResult {
+    api_key: &str,
+    player_settings: PlayerSettings,
+}
+
 pub fn dummy() {
 }
 
 #[test]
 fn dummy_test() {
+    assert!(1i == 2i);
 }
