8c64ae94d34b99943449e3b57328801e523d005b
Paul Hendry <paul@pshendry.com>
2014-10-13 23:35:22 -0400

    Partial work on game info API

--- src/curl.rs
+++ src/curl.rs
@@ -1,23 +1,25 @@
 extern crate http;
 extern crate url;
 
-use self::http::client::RequestWriter;
+use self::http::client::request::RequestWriter;
+use self::http::client::response::ResponseReader;
 use self::http::method::Get;
 use self::url::Url;
-use std::io::IoResult;
+use std::io;
 
-pub fn http_get(url: &str) -> IoResult<String> {
+pub fn http_get(url: &str) -> io::IoResult<ResponseReader> {
     let url_obj = Url::parse(url).unwrap();
     let request: RequestWriter = try!(RequestWriter::new(Get, url_obj));
-    let mut response = try!(request.read_response().map_err(|(_request, error)| error));
-    response.read_to_string()
+    request.read_response().map_err(|(_request, error)| error)
 }
 
-pub fn http_post(url: &str, data: &[u8]) -> IoResult<String> {
+pub fn http_post<'a>(url: &str, data: &[u8]) -> io::IoResult<Box<Reader + 'a>> {
     let url_obj = Url::parse(url).unwrap();
     let mut request: RequestWriter = try!(RequestWriter::new(Get, url_obj));
     request.headers.content_length = Some(data.len());
     try!(request.write(data));
-    let mut response = try!(request.read_response().map_err(|(_request, error)| error));
-    response.read_to_string()
+    match request.read_response() {
+        Ok(response) => Ok(box response as Box<Reader>),
+        Err((_request, error)) => Err(error),
+    }
 }
